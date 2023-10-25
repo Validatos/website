4e382bac831408339b0fc60adef60a10d92afb09
Adrian Covarrubias <adriancova.95@gmail.com>
2023-10-25 09:55:20 -0700

    keep npm specifiers in import map

--- twind.config.ts
+++ twind.config.ts
@@ -1,14 +1,11 @@
 import { Options } from "$fresh/plugins/twindv1.ts";
-import { defineConfig, Preset } from "npm:@twind/core@1.1.3";
-// twind preset
-import presetTailWind from "npm:@twind/preset-tailwind@1.1.4/base";
-import * as colors from "npm:@twind/preset-tailwind@1.1.4/colors";
-import presetAutoPrefix from "npm:@twind/preset-autoprefix@1.0.7";
+import { defineConfig, Preset } from "@twind/core";
+import presetTailWind from "@twind/preset-tailwind/base";
+import * as colors from "@twind/preset-tailwind/colors";
+import presetAutoPrefix from "@twind/preset-autoprefix";
 
-/** @todo Remove the need for type-assertions */
 export default {
   selfURL: import.meta.url,
-  // <BaseTheme, Preset<any>[]>
   ...defineConfig({
     theme: {
       container: {
@@ -40,7 +37,6 @@ export default {
         colors: {
           // This line is required. Otherwise, if removed, the values of other colors with be removed.
           ...colors,
-          // Modify primary and secondary colors according to your color-scheme
           primary: "#be185d",
           secondary: "#4338ca",
           mygray: "#637381",
