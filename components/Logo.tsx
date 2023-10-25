39a5741a70d22026f3559a22d8adcd368fba31cd
Adrian Covarrubias <adriancova.95@gmail.com>
2023-10-25 12:04:08 -0700

    new logo!

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,16 @@
 export default function Logo({ darkTheme = true }) {
   return (
-    <a class="my-navbar flex flex-row items-center py-4" href="/#">
+    <a class="my-navbar flex flex-row items-center py-2" href="/#">
       <img
-        class="h-12 mr-4 md:h-16"
-        src={darkTheme ? "images/logo/logo.svg" : "images/logo/logo2.webp"}
+        class="h-20 mr-4 lg:h-24"
+        src={darkTheme
+          ? "images/logo/logo.png"
+          : "images/logo/logo_transparent.png"}
         alt="Logo"
       />
-      <h1 class={` text-lg md:text-2xl ${darkTheme ? "text-white" : ""}`}>
+      <h1
+        class={`text-2xl lg:text-4xl ${darkTheme ? "text-white" : ""}`}
+      >
         Validatos
       </h1>
     </a>
