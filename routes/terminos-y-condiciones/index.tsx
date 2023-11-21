0d4b2bee27911eb9ec4172ebd5d12aee4914f39e
Adrian Covarrubias <adriancova.95@gmail.com>
2023-11-20 21:45:47 -0800

    pretty t&c

--- routes/terminos-y-condiciones/index.tsx
+++ routes/terminos-y-condiciones/index.tsx
@@ -1,72 +1,78 @@
 const TermsAndConditions = () => (
-  <div>
-    <h2>Términos y Condiciones para Validatos</h2>
-    <div>
-      <h3>1. Introducción</h3>
-      <p>
+  <div className="p-4 md:p-8">
+    <h2 className="text-2xl font-bold mb-4">
+      Términos y Condiciones para Validatos
+    </h2>
+    <div className="space-y-4">
+      <h3 className="text-xl font-semibold">1. Introducción</h3>
+      <p className="text-base">
         Bienvenido a Validatos. Estos términos y condiciones describen las
         reglas y regulaciones para el uso de nuestra plataforma de validación de
         datos KYC.
       </p>
 
-      <h3>2. Definiciones</h3>
-      <p>
+      <h3 className="text-xl font-semibold">2. Definiciones</h3>
+      <p className="text-base">
         Usuario: Cualquier persona que acceda y utilice la plataforma Validatos.
         Servicios KYC: Procesos de validación de identidad utilizando bases de
         datos públicas. Suscripción: Acuerdo comercial para el uso de Validatos
         basado en la selección de un plan periódico de pago.
       </p>
 
-      <h3>3. Aceptación de los Términos</h3>
-      <p>
+      <h3 className="text-xl font-semibold">3. Aceptación de los Términos</h3>
+      <p className="text-base">
         Al acceder y utilizar Validatos, usted acuerda estar vinculado por estos
         términos y condiciones, todas las leyes y regulaciones aplicables, y
         acuerda que es responsable del cumplimiento de cualquier ley local
         aplicable.
       </p>
 
-      <h3>4. Descripción del Servicio</h3>
-      <p>
+      <h3 className="text-xl font-semibold">4. Descripción del Servicio</h3>
+      <p className="text-base">
         Validatos proporciona una plataforma para facilitar la verificación de
         datos de identidad mediante el acceso a servicios públicos de
         validación. No almacenamos ningún dato personal.
       </p>
 
-      <h3>5. Suscripciones</h3>
-      <p>
+      <h3 className="text-xl font-semibold">5. Suscripciones</h3>
+      <p className="text-base">
         Los servicios de Validatos están disponibles bajo planes de suscripción.
         El pago se realizará de acuerdo con el plan seleccionado y las tarifas
         vigentes.
       </p>
 
-      <h3>6. Uso del Servicio</h3>
-      <p>
+      <h3 className="text-xl font-semibold">6. Uso del Servicio</h3>
+      <p className="text-base">
         El usuario se compromete a utilizar los servicios de Validatos
         únicamente para fines legales. El usuario es responsable de obtener el
         consentimiento necesario de las personas cuyos datos se están
         verificando.
       </p>
 
-      <h3>7. Exención de Responsabilidad</h3>
-      <p>
+      <h3 className="text-xl font-semibold">7. Exención de Responsabilidad</h3>
+      <p className="text-base">
         Los servicios se proporcionan "tal cual". Validatos no garantiza la
         precisión o la disponibilidad de los servicios de validación.
       </p>
 
-      <h3>8. Limitación de Responsabilidad</h3>
-      <p>
+      <h3 className="text-xl font-semibold">
+        8. Limitación de Responsabilidad
+      </h3>
+      <p className="text-base">
         Validatos no será responsable por daños indirectos, incidentales o
         consecuentes.
       </p>
 
-      <h3>9. Modificaciones a los Términos</h3>
-      <p>
+      <h3 className="text-xl font-semibold">
+        9. Modificaciones a los Términos
+      </h3>
+      <p className="text-base">
         Validatos se reserva el derecho de modificar estos términos en cualquier
         momento.
       </p>
 
-      <h3>10. Contacto</h3>
-      <p>
+      <h3 className="text-xl font-semibold">10. Contacto</h3>
+      <p className="text-base">
         Para cualquier pregunta o inquietud sobre estos términos, por favor
         contacte al administrador de la plataforma.
       </p>
