0a6966f049855a61d189e84a4a20eab8bbb844ee
Adrian Covarrubias <adriancova.95@gmail.com>
2023-11-20 21:37:27 -0800

    terms and conditions

---
+++ routes/terminos-y-condiciones/index.tsx
@@ -0,0 +1,77 @@
+const TermsAndConditions = () => (
+  <div>
+    <h2>Términos y Condiciones para Validatos</h2>
+    <div>
+      <h3>1. Introducción</h3>
+      <p>
+        Bienvenido a Validatos. Estos términos y condiciones describen las
+        reglas y regulaciones para el uso de nuestra plataforma de validación de
+        datos KYC.
+      </p>
+
+      <h3>2. Definiciones</h3>
+      <p>
+        Usuario: Cualquier persona que acceda y utilice la plataforma Validatos.
+        Servicios KYC: Procesos de validación de identidad utilizando bases de
+        datos públicas. Suscripción: Acuerdo comercial para el uso de Validatos
+        basado en la selección de un plan periódico de pago.
+      </p>
+
+      <h3>3. Aceptación de los Términos</h3>
+      <p>
+        Al acceder y utilizar Validatos, usted acuerda estar vinculado por estos
+        términos y condiciones, todas las leyes y regulaciones aplicables, y
+        acuerda que es responsable del cumplimiento de cualquier ley local
+        aplicable.
+      </p>
+
+      <h3>4. Descripción del Servicio</h3>
+      <p>
+        Validatos proporciona una plataforma para facilitar la verificación de
+        datos de identidad mediante el acceso a servicios públicos de
+        validación. No almacenamos ningún dato personal.
+      </p>
+
+      <h3>5. Suscripciones</h3>
+      <p>
+        Los servicios de Validatos están disponibles bajo planes de suscripción.
+        El pago se realizará de acuerdo con el plan seleccionado y las tarifas
+        vigentes.
+      </p>
+
+      <h3>6. Uso del Servicio</h3>
+      <p>
+        El usuario se compromete a utilizar los servicios de Validatos
+        únicamente para fines legales. El usuario es responsable de obtener el
+        consentimiento necesario de las personas cuyos datos se están
+        verificando.
+      </p>
+
+      <h3>7. Exención de Responsabilidad</h3>
+      <p>
+        Los servicios se proporcionan "tal cual". Validatos no garantiza la
+        precisión o la disponibilidad de los servicios de validación.
+      </p>
+
+      <h3>8. Limitación de Responsabilidad</h3>
+      <p>
+        Validatos no será responsable por daños indirectos, incidentales o
+        consecuentes.
+      </p>
+
+      <h3>9. Modificaciones a los Términos</h3>
+      <p>
+        Validatos se reserva el derecho de modificar estos términos en cualquier
+        momento.
+      </p>
+
+      <h3>10. Contacto</h3>
+      <p>
+        Para cualquier pregunta o inquietud sobre estos términos, por favor
+        contacte al administrador de la plataforma.
+      </p>
+    </div>
+  </div>
+);
+
+export default TermsAndConditions;
