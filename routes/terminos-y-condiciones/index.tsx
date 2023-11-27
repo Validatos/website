a490c33801453f029abd448fd5366bb64c512f80
Adrian Covarrubias <adriancova.95@gmail.com>
2023-11-26 19:09:20 -0800

    updated terminos y condiciones

--- routes/terminos-y-condiciones/index.tsx
+++ routes/terminos-y-condiciones/index.tsx
@@ -1,83 +1,150 @@
-const TermsAndConditions = () => (
-  <div className="p-4 md:p-8">
-    <h2 className="text-2xl font-bold mb-4">
-      Términos y Condiciones para Validatos
-    </h2>
-    <div className="space-y-4">
-      <h3 className="text-xl font-semibold">1. Introducción</h3>
-      <p className="text-base">
-        Bienvenido a Validatos. Estos términos y condiciones describen las
-        reglas y regulaciones para el uso de nuestra plataforma de validación de
-        datos KYC.
-      </p>
+const TermsAndConditions = () => {
+  return (
+    <div className="p-4 md:p-8">
+      <h2 className="text-2xl font-bold mb-4">
+        Términos y Condiciones para Validatos
+      </h2>
+      <div className="space-y-4">
+        <h3 className="text-xl font-semibold">1. Introducción</h3>
+        <p className="text-base">
+          Bienvenido a Validatos. Estos términos y condiciones describen las
+          reglas y regulaciones para el uso de nuestra plataforma de validación
+          de datos KYC.
+        </p>
 
-      <h3 className="text-xl font-semibold">2. Definiciones</h3>
-      <p className="text-base">
-        Usuario: Cualquier persona que acceda y utilice la plataforma Validatos.
-        Servicios KYC: Procesos de validación de identidad utilizando bases de
-        datos públicas. Suscripción: Acuerdo comercial para el uso de Validatos
-        basado en la selección de un plan periódico de pago.
-      </p>
+        <h3 className="text-xl font-semibold">2. Definiciones</h3>
+        <p className="text-base">
+          Usuario: Cualquier persona que acceda y utilice la plataforma
+          Validatos. Servicios KYC: Procesos de validación de identidad
+          utilizando bases de datos públicas. Suscripción: Acuerdo comercial
+          para el uso de Validatos basado en la selección de un plan periódico
+          de pago.
+        </p>
 
-      <h3 className="text-xl font-semibold">3. Aceptación de los Términos</h3>
-      <p className="text-base">
-        Al acceder y utilizar Validatos, usted acuerda estar vinculado por estos
-        términos y condiciones, todas las leyes y regulaciones aplicables, y
-        acuerda que es responsable del cumplimiento de cualquier ley local
-        aplicable.
-      </p>
+        <h3 className="text-xl font-semibold">3. Aceptación de los Términos</h3>
+        <p className="text-base">
+          Al acceder y utilizar Validatos, usted acuerda estar vinculado por
+          estos términos y condiciones, todas las leyes y regulaciones
+          aplicables, y acuerda que es responsable del cumplimiento de cualquier
+          ley local aplicable.
+        </p>
 
-      <h3 className="text-xl font-semibold">4. Descripción del Servicio</h3>
-      <p className="text-base">
-        Validatos proporciona una plataforma para facilitar la verificación de
-        datos de identidad mediante el acceso a servicios públicos de
-        validación. No almacenamos ningún dato personal.
-      </p>
+        <h3 className="text-xl font-semibold">4. Descripción del Servicio</h3>
+        <p className="text-base">
+          Validatos proporciona una plataforma para facilitar la verificación de
+          datos de identidad mediante el acceso a servicios públicos de
+          validación. No almacenamos ningún dato personal.
+        </p>
 
-      <h3 className="text-xl font-semibold">5. Suscripciones</h3>
-      <p className="text-base">
-        Los servicios de Validatos están disponibles bajo planes de suscripción.
-        El pago se realizará de acuerdo con el plan seleccionado y las tarifas
-        vigentes.
-      </p>
+        <h3 className="text-xl font-semibold">5. Suscripciones</h3>
+        <p className="text-base">
+          Los servicios de Validatos están disponibles bajo planes de
+          suscripción. El pago se realizará de acuerdo con el plan seleccionado
+          y las tarifas vigentes.
+        </p>
 
-      <h3 className="text-xl font-semibold">6. Uso del Servicio</h3>
-      <p className="text-base">
-        El usuario se compromete a utilizar los servicios de Validatos
-        únicamente para fines legales. El usuario es responsable de obtener el
-        consentimiento necesario de las personas cuyos datos se están
-        verificando.
-      </p>
+        <h3 className="text-xl font-semibold">6. Suscripciones y Pagos</h3>
+        <p className="text-base">
+          Los servicios de Validatos se ofrecen bajo diferentes planes de
+          suscripción con pagos recurrentes. Los pagos y la gestión de las
+          suscripciones se realizan a través del proveedor de servicios de pago
+          de terceros, Stripe. Al suscribirse, usted acuerda estar vinculado por
+          los términos y condiciones de Stripe, además de los presentes
+          términos. La información de facturación y pago se manejará
+          directamente en la plataforma de Stripe, y Validatos no tendrá acceso
+          a los detalles completos de su tarjeta de crédito o cuenta bancaria.
+          Validatos se reserva el derecho de cambiar los precios o los planes de
+          suscripción. Cualquier cambio en los precios se comunicará a los
+          usuarios con antelación razonable. La facturación se realiza de forma
+          anticipada en cada ciclo de facturación según el plan seleccionado.
+        </p>
 
-      <h3 className="text-xl font-semibold">7. Exención de Responsabilidad</h3>
-      <p className="text-base">
-        Los servicios se proporcionan "tal cual". Validatos no garantiza la
-        precisión o la disponibilidad de los servicios de validación.
-      </p>
+        <h3 className="text-xl font-semibold">7. Política de Reembolso</h3>
+        <p className="text-base">
+          Los reembolsos por los servicios suscritos a través de Validatos
+          pueden estar sujetos a la política de reembolsos de Stripe. Validatos
+          evaluará las solicitudes de reembolso caso por caso y se reservará el
+          derecho de conceder o negar la solicitud de reembolso a su discreción.
+          Si se concede un reembolso, este se realizará utilizando el mismo
+          método de pago utilizado para la compra.
+        </p>
 
-      <h3 className="text-xl font-semibold">
-        8. Limitación de Responsabilidad
-      </h3>
-      <p className="text-base">
-        Validatos no será responsable por daños indirectos, incidentales o
-        consecuentes.
-      </p>
+        <h3 className="text-xl font-semibold">8. Cancelación de Suscripción</h3>
+        <p className="text-base">
+          Los usuarios pueden cancelar su suscripción en cualquier momento a
+          través de su panel de configuración de cuenta en Validatos o
+          directamente a través de Stripe. La cancelación se hará efectiva al
+          final del ciclo de facturación actual, y no se cobrarán más pagos
+          después de la cancelación.
+        </p>
 
-      <h3 className="text-xl font-semibold">
-        9. Modificaciones a los Términos
-      </h3>
-      <p className="text-base">
-        Validatos se reserva el derecho de modificar estos términos en cualquier
-        momento.
-      </p>
+        <h3 className="text-xl font-semibold">
+          9. Exención de Responsabilidad sobre Servicios de Terceros
+        </h3>
+        <p className="text-base">
+          Validatos no es responsable de las interrupciones o fallos en los
+          servicios proporcionados por Stripe o cualquier otro proveedor de
+          servicios de terceros.
+        </p>
 
-      <h3 className="text-xl font-semibold">10. Contacto</h3>
-      <p className="text-base">
-        Para cualquier pregunta o inquietud sobre estos términos, por favor
-        contacte al administrador de la plataforma.
-      </p>
+        <h3 className="text-xl font-semibold">
+          10. Derechos de Autor y Propiedad Intelectual
+        </h3>
+        <p className="text-base">
+          Todo el contenido de Validatos, incluyendo logos, textos, gráficos y
+          código de software, es propiedad de la plataforma y está protegido por
+          leyes de derechos de autor y tratados internacionales. Los usuarios no
+          pueden usar, reproducir o distribuir cualquier contenido de la
+          plataforma sin el consentimiento explícito del administrador de
+          Validatos.
+        </p>
+
+        <h3 className="text-xl font-semibold">11. Terminación</h3>
+        <p className="text-base">
+          Validatos se reserva el derecho de terminar su acceso a la plataforma
+          por cualquier violación de estos términos y condiciones, con o sin
+          previo aviso.
+        </p>
+
+        <h3 className="text-xl font-semibold">12. Cambios a la Plataforma</h3>
+        <p className="text-base">
+          Validatos se reserva el derecho de modificar o discontinuar, temporal
+          o permanentemente, la plataforma o cualquier parte de la misma con o
+          sin previo aviso.
+        </p>
+
+        <h3 className="text-xl font-semibold">13. Ley Aplicable</h3>
+        <p className="text-base">
+          Estos términos y condiciones se regirán e interpretarán de acuerdo con
+          las leyes del país o estado desde donde se administra la plataforma.
+        </p>
+
+        <h3 className="text-xl font-semibold">14. Disposiciones Generales</h3>
+        <p className="text-base">
+          Si cualquier parte de estos términos se considera no válida o
+          inaplicable, esa parte se interpretará de una manera coherente con la
+          ley aplicable para reflejar, lo más cercano posible, las intenciones
+          originales de las partes, y las partes restantes permanecerán en plena
+          vigencia y efecto.
+        </p>
+
+        <h3 className="text-xl font-semibold">15. Contacto</h3>
+        <p className="text-base">
+          Para cualquier pregunta o inquietud sobre estos términos, por favor
+          contacte al administrador de la plataforma a través de [método de
+          contacto].
+        </p>
+
+        <h3 className="text-xl font-semibold">
+          16. Aceptación de los Términos
+        </h3>
+        <p className="text-base">
+          Al utilizar la plataforma, usted acepta estar sujeto a estos términos
+          y condiciones.
+        </p>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default TermsAndConditions;
